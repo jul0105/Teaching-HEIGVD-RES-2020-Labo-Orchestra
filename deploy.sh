6850a63f8ccb9942c929695a5b243f9e2ae1ebf3
Gil Balsiger <gil.balsiger@gmail.com>
2020-06-26 22:41:32 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,5 @@ done
 
 echo ""
 echo "You can stop every containers with 'docker-compose down'"
+echo "You can stop musicians with 'docker stop \$(docker ps -q --filter ancestor=res/musician)'"
 echo "You can start a new musician with 'docker-compose run -d --rm musician <instrument>'"
